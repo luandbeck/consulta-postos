669174db4d91f54294d72b3ef4b46b1bed99049c
luandbeck <luanbeck@gmail.com>
2021-09-18 08:41:24 -0300

    First commit

---
+++ src/main/java/com/faculdade/consultapostos/ConsultaPostosApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.consultapostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaPostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaPostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faculdade/consultapostos/ConsultaPostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.consultapostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaPostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
